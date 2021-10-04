0dd88066bace1d41f4122e1d25b7b2ce5851a4c8
Jeremiah <jerrycasuga9@gmail.com>
2021-10-04 17:13:04 +1000

    Updated main code

--- catkin_ws/src/robot_hub/src/main.py
+++ catkin_ws/src/robot_hub/src/main.py
@@ -1,6 +1,30 @@
 #! /usr/bin/env python
 
-import rospy
+# ------------------------------------------
+# Main hub for the logic behind the robot
+#
+# Written by Jeremiah Casuga
+# ------------------------------------------
 
-rospy.init_node('Obiwan')
-print("help me obi-wan kenobi, you're my only hope")
+import rospy
+import modern_robotics as mr
+from sensor_msgs.msg import JointState
+
+
+def move_servo():
+    """Function that takes desired joint state information and publishes
+    to the topic  
+
+    :param states: Desired joint angles
+    :return: none
+    """
+    rospy.init_node('main_loop')
+    servo_publisher = rospy.Publisher('desired_joint_states', JointState, queue_size = 1)
+
+    servo_publisher.publish([], ['joint_1'], [1], [0.5], [])
+    print("Message successfully published")
+    rospy.spin()
+
+if __name__ == '__main__':
+    move_servo()
+    
