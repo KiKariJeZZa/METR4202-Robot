df2e8bf05efeaa6cda3faeb2d174ba077d0fdefe
Jeremiah <jerrycasuga9@gmail.com>
2021-10-04 17:43:25 +1000

    zero pos implemented

--- catkin_ws/src/robot_hub/src/main.py
+++ catkin_ws/src/robot_hub/src/main.py
@@ -11,12 +11,13 @@ import rospy
 import modern_robotics as mr
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
+import numpy as np
 
-def move_servo():
+def move_servo(des_pos):
     """Function that takes desired joint state information and publishes
     to the topic  
 
-    :param states: Desired joint angles
+    :param des_pos: Desired joint angles list
     :return: none
     """
     rospy.init_node('main_loop')
@@ -27,7 +28,7 @@ def move_servo():
     state_str.header = Header()
     state_str.header.stamp = rospy.Time.now()
     state_str.name = ['joint_1','joint_2','joint_3','joint_4']
-    state_str.position = [0,1.5,0,0]
+    state_str.position = [des_pos[0],des_pos[1],des_pos[2],des_pos[3]]
     state_str.velocity = [0.5,0.5,0.5,0.5]
     state_str.effort = []
     while not rospy.is_shutdown():
@@ -37,6 +38,16 @@ def move_servo():
         print("Message successfully published")
         rate.sleep()
 
+def zero_position():
+    """ Function that returns the robot configuration to its zero position
+
+    :param: none
+    :return: array of zero position
+    """
+    return np.array([0,1.5,0,0])
+
+
+
 if __name__ == '__main__':
-    move_servo()
+    move_servo(zero_position())
     
