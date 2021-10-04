73c00950de947c1a225a6fea02bfaa88b9db9274
Jeremiah <jerrycasuga9@gmail.com>
2021-10-04 18:04:16 +1000

    Fixed bug

--- catkin_ws/src/robot_hub/src/main.py
+++ catkin_ws/src/robot_hub/src/main.py
@@ -21,7 +21,7 @@ def move_servo(des_pos):
     :return: none
     """
     rospy.init_node('main_loop')
-    servo_publisher = rospy.Publisher('desired_joint_states', JointState, queue_size = 10)
+    servo_publisher = rospy.Publisher('/desired_joint_states', JointState, queue_size = 10)
     rate = rospy.Rate(10) # 10hz
     # Build the required string needed to send to the topic
     state_str = JointState()
@@ -35,8 +35,11 @@ def move_servo(des_pos):
         state_str.header.stamp = rospy.Time.now()
         # Send to topic
         servo_publisher.publish(state_str)
-        print("Message successfully published")
+        #print("Message successfully published")
         rate.sleep()
+        if JointState().position == state_str.position :
+            print(state_str)
+            rospy.shutDown()
 
 def zero_position():
     """ Function that returns the robot configuration to its zero position
@@ -45,8 +48,6 @@ def zero_position():
     :return: array of zero position
     """
     return np.array([0,1.5,0,0])
-
-
 
 if __name__ == '__main__':
     move_servo(zero_position())
