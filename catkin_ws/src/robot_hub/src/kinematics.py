30ea5beab66f05a27a6abefd06581b554014d9f1
Jeremiah <jerrycasuga9@gmail.com>
2021-10-09 14:08:36 +1000

    Barebone transformations done

---
+++ catkin_ws/src/robot_hub/src/kinematics.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+
+# ------------------------------------------
+# Kinematics hub for the logic behind the robot
+#
+# Written by Jeremiah Casuga
+# Assisted by Nadia Regli
+# ------------------------------------------
+
+import modern_robotics as mr
+import numpy as np
+import rospy
+
+def end_effector_pos(M, Slist, thetalist):
+    """
+    Returns the forward kinematics of the end effector in space
+    """
+    return FKinSpace(M, Slist, thetalist)
+
+def desired_angle_config(Slist, M, thetalist):
+    """
+    Returns an array of desired joint configurations for the robot to move to
+    """
+    # Set initial error tolerances
+    eomg = 0.001
+    ev = 0.001
+    for i in range(220, 280):
+        theta = i * np.pi / 180
+        # T will be arbitrary for now
+        T = np.array([[np.cos(theta), -np.sin(theta), 0, 0.1],
+                    [np.sin(theta), np.cos(theta), 0, 0.03],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])   
+        outcome = mr.IKinSpace(Slist, M, T, thetalist, eomg, ev)
+        joint1 = np.degrees(outcome[0][0])
+        joint2 = np.degrees(outcome[0][1])
+        joint3 = np.degrees(outcome[0][2])
+        joint4 = np.degrees(outcome[0][3])
+        if outcome[1] == True:
+            if -90 <= joint1 <= 90:
+                if -90 <= joint2 <= 90:
+                    if -90 <= joint3 <= 90:
+                        joint1 = joint1 * np.pi / 180
+                        joint2 = joint2 * np.pi / 180
+                        joint3 = joint3 * np.pi / 180
+                        joint4 = joint4 * np.pi / 180
+                        if -0.0001 <= joint1 <= 0.0001:
+                            joint1 = 0
+                        if -0.0001 <= joint2 <= 0.0001:
+                            joint2 = 0
+                        if -0.0001 <= joint3 <= 0.0001:
+                            joint3 = 0
+                        if -0.0001 <= joint4 <= 0.0001:
+                            joint4 = 0
+                        print(f'Joint1 Angle: {joint1}, Joint2 Angle: {joint2}, Joint3 Angle: {joint3}, Joint4 Angle: {joint4}')
+                        return np.array([joint1, joint2, joint3, joint4])
+                        break
--- catkin_ws/src/robot_hub/src/main.py
+++ catkin_ws/src/robot_hub/src/main.py
@@ -12,13 +12,24 @@ import modern_robotics as mr
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 import numpy as np
+import kinematics as k
+
+# Declare variables for the robot
+joint2_offset = 1.5
+M = np.array([[1, 0, 0, 0.0565],
+            [0, 1, 0, 0.205],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]])
+Slist = np.array([[0, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0.03, 0, 0],
+                [0, 0, 1, 0.12, 0, 0],
+                [1, 0, 0, 0, 0, -0.205]]).T
+thetalist = np.array([0, 0, 0, 0])
 
 def move_servo(des_pos):
-    """Function that takes desired joint state information and publishes
+    """
+    Function that takes desired joint state information and publishes
     to the topic  
-
-    :param des_pos: Desired joint angles list
-    :return: none
     """
     rospy.init_node('main_loop')
     servo_publisher = rospy.Publisher('/desired_joint_states', JointState, queue_size = 10)
@@ -28,27 +39,28 @@ def move_servo(des_pos):
     state_str.header = Header()
     state_str.header.stamp = rospy.Time.now()
     state_str.name = ['joint_1','joint_2','joint_3','joint_4']
-    state_str.position = [des_pos[0],des_pos[1],des_pos[2],des_pos[3]]
-    state_str.velocity = [0.5,0.5,0.5,0.5]
+    state_str.position = [des_pos[0],des_pos[1]+joint2_offset,des_pos[2],des_pos[3]]
+    state_str.velocity = [1,1,1,1]
     state_str.effort = []
-    while not rospy.is_shutdown():
+    count = 0
+    print("Moving to desired position")
+    while count < 50:
         state_str.header.stamp = rospy.Time.now()
         # Send to topic
         servo_publisher.publish(state_str)
         #print("Message successfully published")
         rate.sleep()
-        if JointState().position == state_str.position :
-            print(state_str)
-            rospy.shutDown()
+        count = count + 1
+        
 
 def zero_position():
-    """ Function that returns the robot configuration to its zero position
-
-    :param: none
-    :return: array of zero position
+    """ 
+    Function that returns the robot configuration to its zero position
     """
-    return np.array([0,1.5,0,0])
+    print("Moving to zero position")
+    return np.array([0,0,0,0])
 
 if __name__ == '__main__':
+    move_servo(k.desired_angle_config(Slist, M, thetalist))
     move_servo(zero_position())
     
